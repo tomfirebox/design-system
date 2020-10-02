5ed8dab3fd7473fdb127457dd4e99df51e12bfcc
Tom Penfold <tom.penfold@icloud.com>
2020-10-02 16:02:26 +0100

    Added normalize

--- src/Components/_Base/RootBase.js
+++ src/Components/_Base/RootBase.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "emotion-theming";
 import { theme } from "../";
+import "normalize.css";
 
 export const RootBase = ({ children }) => (
   <ThemeProvider theme={theme}>{children}</ThemeProvider>
