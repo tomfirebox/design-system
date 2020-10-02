9c36762e4488e152d139e04f13bc5ce91d7dd98a
Tom Penfold <tom.penfold@icloud.com>
2020-10-02 17:07:51 +0100

    Added TypeBlock WIP

--- src/Components/1_Atoms/Type/TypeBlock.js
+++ src/Components/1_Atoms/Type/TypeBlock.js
@@ -1,4 +1,12 @@
 import styled from "@emotion/styled";
-export const TypeBlock = styled.div`
-  background: red;
-`;
+import css from "@styled-system/css";
+
+export const TypeBlock = styled.div(
+  css({
+    h1: {
+      fontSize: ["1rem", "3rem"],
+      fontWeight: "primary.bold",
+      mb: ["1rem"],
+    },
+  })
+);
