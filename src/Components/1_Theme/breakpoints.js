4d17b445ed5b098688c896b8e5088b973bb1e39a
Tom Penfold <tom.penfold@icloud.com>
2020-10-06 12:37:06 +0100

    Tweaks breakpoints

--- src/Components/1_Theme/breakpoints.js
+++ src/Components/1_Theme/breakpoints.js
@@ -1,5 +1,5 @@
-export const breakpoints = ["576px", "768px", "992px", "1200px", "1350px"];
-export const containerPoints = ["540px", "720px", "960px", "1140px", "1240px"];
+export const breakpoints = ["576px", "768px", "992px", "1200px", "1450px"];
+export const containerPoints = ["540px", "720px", "960px", "1140px", "1340px"];
 
 breakpoints.xs = breakpoints[0];
 breakpoints.sm = breakpoints[1];
