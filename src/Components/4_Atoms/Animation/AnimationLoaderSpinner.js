ecfd5f63a8d892ca72ad7201441a4f10e10ee4e8
Tom Penfold <tompenfold@toms-mbp.lan>
2021-01-21 21:20:58 +0000

    Added spinner component

---
+++ src/Components/4_Atoms/Animation/AnimationLoaderSpinner.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { Box, HubMarkSelector } from "../../";
+
+export const AnimationLoaderSpinner = ({ delayIn = 0.1, ...props }) => {
+  const loaderinnerVariants = {
+    initial: {
+      scale: 0.8,
+      opacity: 0,
+    },
+    animate: {
+      scale: 1,
+      opacity: 1,
+      transition: { delay: delayIn },
+    },
+    exit: {
+      scale: 0,
+      opacity: 0,
+    },
+  };
+  return (
+    <Box
+      as={motion.div}
+      variants={loaderinnerVariants}
+      animate="animate"
+      exit="exit"
+      size="6rem"
+      {...props}
+    >
+      <motion.div
+        animate={{ rotate: 720 }}
+        transition={{ duration: 2, repeat: Infinity }}
+      >
+        <HubMarkSelector />
+      </motion.div>
+    </Box>
+  );
+};
